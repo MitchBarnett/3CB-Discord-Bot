b2e66352510f818b329ebe164a27d04af4c2a88a
Mitch Barnett <mitch@mitchbarnett.com>
2018-06-02 20:27:06 +0100

    Added query commands for online players and current mission

--- bot.py
+++ bot.py
@@ -1,12 +1,11 @@
 import discord
 import asyncio
 import config
-import serverinfo
+import serverInfo
 
 def registerCommands(client):
     @client.event
     async def on_ready():
-        client.servers
         print("Running discord.py version: " + discord.__version__)
         print('------')
         print('Logged in as: ' + str(client.user))
@@ -18,5 +17,17 @@ def registerCommands(client):
 
     @client.event
     async def on_message(message):
-        if message.content.startswith('!test'):
-            await client.send_message(message.channel, "test")
+        if message.content.startswith("!mission"):
+            info = serverInfo.getInfo()
+            await client.send_message(message.channel, "The current mission is %s on %s" % (info.game, info.map))
+
+        elif message.content.startswith("!players"):
+            info = serverInfo.getInfo()
+            await client.send_message(message.channel, "There is currently %i players online" % info.player_count)
+
+        elif message.content.startswith("!who"):
+            players = serverInfo.getPlayers()
+            embed = discord.Embed(title="Players Online", description="")
+            for player in players.players:
+                embed.add_field(name=player.name, value= str(int(player.duration / 60)) + " mins" , inline=True)
+            await client.send_message(message.channel, embed=embed)
