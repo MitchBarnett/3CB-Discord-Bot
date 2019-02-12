1cf4e590814273ee0f24db926e5ad6f287a677d4
Mitch Barnett <mitch@mitchbarnett.com>
2019-02-12 11:22:42 +0000

    updated to handle offline server

--- serverInfo.py
+++ serverInfo.py
@@ -4,9 +4,19 @@ def getQueryAdress():
     ip = str(config.getValue("serverIP"))
     port = int(config.getValue("serverPort")) + 1 # query port is +1
     return (ip, port)
+
+
 def getInfo():
-    with valve.source.a2s.ServerQuerier(getQueryAdress()) as server:
-        return server.info()
+    try:
+        with valve.source.a2s.ServerQuerier(getQueryAdress()) as server:
+            return server.info()
+    except valve.source.util.NoResponseError:
+        return None
+
+
 def getPlayers():
-    with valve.source.a2s.ServerQuerier(getQueryAdress()) as server:
-        return server.players()
+    try:
+        with valve.source.a2s.ServerQuerier(getQueryAdress()) as server:
+            return server.players()
+    except valve.source.util.NoResponseError:
+        return None
